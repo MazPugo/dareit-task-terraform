9fed73555262675928c489192bd11662bc81f2d7
MazPugo <94927552+MazPugo@users.noreply.github.com>
2023-04-04 21:43:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
 
+provider "google" {
+  project = "neat-resolver-378118"
+  region  = "europe-west2"
+  zone    = "europe-west2-c"
+}
+
+resource "google_storage_bucket" "my-bucket" {
+  name = "my-bucket"
+  location = "EU"
+  storage_class = "STANDARD"
+} 
+
 
 resource "google_compute_instance" "dareit-vm-ci" {
   name         = "dareit-vm-tf-ci"
   machine_type = "e2-medium
   zone         = "europe-west2-c"
-
   tags = ["dareit"]
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -15,12 +25,10 @@ resource "google_compute_instance" "dareit-vm-ci" {
       }
     }
   }
-
   network_interface {
     network = "default"
-
     access_config {
       // Ephemeral public IP
       }
     }
-  }
+}
