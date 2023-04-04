d041d60ec5fd00be4f2a3ae9699ea3420b9755fb
MazPugo <94927552+MazPugo@users.noreply.github.com>
2023-04-04 21:49:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 
-provider "google" {
-  project = "neat-resolver-378118"
-  region  = "europe-west2"
-  zone    = "europe-west2-c"
-}
-
-resource "google_storage_bucket" "my-bucket" {
-  name = "my-bucket"
-  location = "EU"
-  storage_class = "STANDARD"
-} 
-
 
 resource "google_compute_instance" "dareit-vm-ci" {
   name         = "dareit-vm-tf-ci"
