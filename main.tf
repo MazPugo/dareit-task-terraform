ba425b8526cf05c04bfcc6f9e0db62bd672b0893
MazPugo <94927552+MazPugo@users.noreply.github.com>
2023-04-10 17:28:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 
 resource "google_compute_instance" "dareit-vm-ci" {
-  name = "dareit-vm-tf-ci"
-  machine_type = "e2-medium
-  zone = "europe-west2-c"
+  name         = "dareit-vm-tf-ci"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
   tags = ["dareit"]
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -12,10 +14,13 @@ resource "google_compute_instance" "dareit-vm-ci" {
       }
     }
   }
+
   network_interface {
     network = "default"
+
     access_config {
       // Ephemeral public IP
-      }
     }
+  }
 }
+
