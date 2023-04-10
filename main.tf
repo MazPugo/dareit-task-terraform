31e4c93ecedeca150eb6a268435c53325129f9d2
MazPugo <94927552+MazPugo@users.noreply.github.com>
2023-04-10 20:23:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,8 @@
 
-provider "google" {
-  project = "neat-resolver-378118"
-  region  = "europe-west2"
-  zone    = "europe-west2-c"
-  credentials = var.gcp-creds
+resource "google_storage_bucket" "bucket" {
+  name          = "task_7_terraform_ci"
+  project       = "neat-resolver-378118"
+  storage_class = "standard"
+  location      = "EU"
 }
-
-
-variable "gcp-creds" {
-default= ""
-}
-
-
-resource "google_storage_bucket" "dareit-website" {
-  name = "Maz-website"
-  location = "europe-west2"
-  storage_class = "STANDARD"
-  labels = {
-   "key1" = "value1"
-   "key2" = "value2"
- }
-} 
-
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "europe-west2-c"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
+ 
