da560ac2af67d19f75f6149b40324923de32241a
MazPugo <94927552+MazPugo@users.noreply.github.com>
2023-04-10 16:53:27 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
-    bucket = "mr-terraform-state-file"
+    bucket = "ml-terraform-state-file"
     prefix = "dev"
   }
   required_providers {
